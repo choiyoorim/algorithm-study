6ba70b4e4267c5cf1fb75d975e381c21656cfd17
choiyoorim <yulim3839@naver.com>
2023-07-11 23:35:20 +0900

    [230711] BJ11003

---
+++ BJ11003/src/BJ11003.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class BJ11003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> deque = new LinkedList<>();
+        for(int i = 0; i<N; i++){
+            int now = Integer.parseInt(st.nextToken());
+
+            while(!deque.isEmpty() && deque.getLast().value > now ){
+                deque.removeLast();
+            }
+
+            deque.addLast(new Node(i, now));
+
+            if(deque.getFirst().index <= i - L){
+                deque.removeFirst();
+            }
+            bw.write(deque.getFirst().value + " ");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    static class Node {
+        public int value;
+        public int index;
+
+        Node(int index, int value){
+            this.index = index;
+            this.value = value;
+        }
+    }
+}
