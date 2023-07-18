8dc6102cdb42ab19934602f82927fdf53f650899
choiyoorim <yulim3839@naver.com>
2023-07-18 14:18:47 +0900

    [230718] BJ1927

--- BJ1927/src/BJ1927.java
+++ BJ1927/src/BJ1927.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BJ1927 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BJ1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i = 0; i<N; i++){
+            int request = Integer.parseInt(br.readLine());
+            if(request == 0){
+                if(queue.isEmpty())
+                    System.out.println(0);
+                else
+                    System.out.println(queue.poll());
+            }
+            else{
+                queue.add(request);
+            }
+        }
+    }
 }
