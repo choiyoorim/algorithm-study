2440b36239a859699b367288f96e621bd61949d6
choiyoorim <yulim3839@naver.com>
2023-07-18 14:19:04 +0900

    [230718] BJ2075

--- BJ2075/src/BJ2075.java
+++ BJ2075/src/BJ2075.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BJ2075 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+        for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int k = 0; k<N; k++){
+                queue.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for(int i = 0; i<N-1; i++){
+            queue.poll();
+        }
+        System.out.println(queue.poll());
+    }
 }
