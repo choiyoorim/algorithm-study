4098d22216abcd726259bc5ecf5275e2b370697e
choiyoorim <yulim3839@naver.com>
2023-07-19 11:55:14 +0900

    [230719] BJ2750

--- BJ2750/src/BJ2750.java
+++ BJ2750/src/BJ2750.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class BJ2750 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] a = new int[N];
+        for(int i = 0; i<N; i++){
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i<N-1; i++){
+            for(int j = 0; j<N-1-i; j++){
+                if(a[j] > a[j+1]){
+                    int tmp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+
+        for(int i = 0; i<N; i++){
+            System.out.println(a[i]);
+        }
+    }
 }
