666dd70c544979c2688741e1e8ed39255c5a78b6
choiyoorim <yulim3839@naver.com>
2024-01-24 11:21:41 +0900

    [240124] BJ1747

--- BJ1747/src/BJ1747.java
+++ BJ1747/src/BJ1747.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class BJ1747 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1747 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] A = new int[2000001];
+        for(int i = 2; i < A.length; i++){
+            A[i] = i;
+        }
+
+        for(int i = 2; i < Math.sqrt(A.length); i++){
+            if(A[i] == 0) continue;
+            for(int j = i + i; j < A.length; j = j + i){
+                A[j] = 0;
+            }
+        }
+
+        int i = N;
+        while(true){
+            if(A[i] != 0){
+                if(isPalindrome(A[i])) {
+                    System.out.println(A[i]);
+                    break;
+                }
+            }
+            i++;
+        }
+    }
+
+    private static boolean isPalindrome(int N){
+        char temp[] = String.valueOf(N).toCharArray();
+        int start = 0;
+        int end = temp.length - 1;
+        while(start<end){
+            if(temp[start] != temp[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
