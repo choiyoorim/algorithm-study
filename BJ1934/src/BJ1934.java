4333d8143860c97c3ee82e804a14316c95ca83b8
choiyoorim <yulim3839@naver.com>
2024-01-26 22:15:20 +0900

    [240126] BJ1934

--- BJ1934/src/BJ1934.java
+++ BJ1934/src/BJ1934.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BJ1934 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        for(int i = 0; i<T; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int result = A * B / gcd(A, B);
+            System.out.println(result);
+        }
+
+    }
+
+    private static int gcd(int a, int b){
+        if(b==0) return a;
+        else return gcd(b, a%b);
+    }
 }
