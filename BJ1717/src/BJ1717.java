c3306e6cb40b611baa6fe9ec84f85fd5ba50d840
choiyoorim <yulim3839@naver.com>
2024-02-01 09:02:50 +0900

    [240131] BJ1717

--- BJ1717/src/BJ1717.java
+++ BJ1717/src/BJ1717.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class BJ1717 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1717 {
+    public static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+        for(int i = 0; i<=N; i++){
+            parent[i] = i;
+        }
+        for(int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int q = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(q == 0){
+                union(a, b);
+            }
+            else{
+                if(checkSame(a,b)){
+                    System.out.println("YES");
+                }
+                else{
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a != b){
+            parent[b] = a;
+        }
+    }
+
+    public static int find(int a){
+        if(parent[a] == a) return a;
+        else{
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    public static boolean checkSame(int a, int b){
+        if(find(a) != find(b)) return false;
+        else return true;
+    }
 }
